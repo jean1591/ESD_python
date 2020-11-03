7620ddac99a91f31c7723176c222d0f13eae1788
Jean Robertou <jean.robertou@whereyoulove.com>
2020-11-03 14:27:02 +0100

    List_mult

---
+++ 20201103/list_sum.py
@@ -0,0 +1,29 @@
+my_list = [1, 2, 3, 4, 5, 6]
+
+
+# LIST SUM
+# Initialisation à 0 de la variable de total
+total_sum = 0
+
+# Parcours des éléments de my_list
+for b in my_list:
+  # B => Ajout de l'élément parcouru à total
+  # print(">>", total)
+  total_sum += b
+
+# Affichage de total
+print(total_sum)
+
+
+# LIST SUM
+# Initialisation à 0 de la variable de total
+total_mult = 1
+
+# Parcours des éléments de my_list
+for b in my_list:
+  # B => Ajout de l'élément parcouru à total
+  # print(">>", total)
+  total_mult *= b
+
+# Affichage de total
+print(total_mult)
