fee79c2385d36f8974a90b8247c42e0c437cf9d9
Jean Robertou <jean.robertou@whereyoulove.com>
2020-11-03 09:07:14 +0100

    20201103 - dct

--- 20201027/mimic_db.py
+++ 20201027/mimic_db.py
@@ -1,7 +1,10 @@
 from random import choice, randint
+import os
 
-print(choice([True, False]))
-print(randint(0, 100))
+os.system("clear")
+
+# print(choice([True, False]))
+# print(randint(0, 100))
 
 cat_list = ["household", "groceries", "travels", "extra", "rent"]
 
@@ -15,3 +18,26 @@ db = [
 ### Savoir faire une enregistrement à la fois
 ### Savoir ajouter l'enregistrement créée dans db
 ### Boucler n fois en créant et ajoutant des enregistrement
+
+
+db = []
+
+# Création d'un dict
+tmp = {
+  "value": randint(1, 1000),
+  "executed": choice([True, False]),
+  "cat": choice(cat_list)
+}
+
+# print(tmp)
+
+db.append(tmp)
+
+print(db)
+
+
+# Faire boucle for
+# Dans boucle => Générer dict tmp
+# Dans boucle + Dict généré => Ajouter dict à db via .append()
+# Fin boucle
+# Afficher db via print()
