f64e3c273072478ef4c487640ccc43f4b2652e1e
Jean Robertou <jean.robertou@whereyoulove.com>
2020-11-03 11:17:54 +0100

    Gym DB finished

--- 20201103/gym_db.py
+++ 20201103/gym_db.py
@@ -1,6 +1,9 @@
 import datetime
 from random import randint, choice
 from os import system
+from pprint import pprint
+import pandas as pd
+
 
 system("clear")
 
@@ -21,58 +24,47 @@ domains = ["gmail.com", "example.com", "outlook.com", "mailo.com"]
 programs = ["basic", "premium"]
 locations = ["sydney", "camberra", "perth", "darwin"]
 
-db = []
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Random name
-tmp_name = choice(names)
-
-# Random email
-tmp_email = tmp_name + "@example.com"
-
-# Random last payment date
 # Generate random date between two bounds
 start_date = datetime.date(2020, 1, 1)
 end_date = datetime.date(2020, 12, 31)
-
 time_between_dates = end_date - start_date
 days_between_dates = time_between_dates.days
-random_number_of_days = randint(0, days_between_dates)
-""" print("time_between_dates", time_between_dates)
-print("days_between_dates", days_between_dates)
-print("random_number_of_days", random_number_of_days) """
-
-tmp_last_payment = start_date + datetime.timedelta(days=random_number_of_days)
-
-random_number_of_days = randint(0, days_between_dates)
-# print("random_number_of_days", random_number_of_days)
-tmp_last_session = start_date + datetime.timedelta(days=random_number_of_days)
 
 
-tmp_name = choice(names)
-tmp = {
-  "name": tmp_name.lower(),
-  "email": tmp_name.lower() + "@" + choice(domains),
-  "last_payment": tmp_last_payment.strftime("%Y-%m-%d"),
-  "last_session": tmp_last_session.strftime("%Y-%m-%d"),
-  "program": choice(programs),
-  "sponsorship": choice([True, False]),
-  "location": choice(locations)
-}
 
-print(tmp)
+
+db = []
+
+# Début boucle for
+for i in range(10):
+
+  # Last payment date
+  random_number_of_days = randint(0, days_between_dates)
+  # print("random_number_of_days", random_number_of_days) 
+  tmp_last_payment = start_date + datetime.timedelta(days=random_number_of_days)
+
+  # Last session date
+  random_number_of_days = randint(0, days_between_dates)
+  # print("random_number_of_days", random_number_of_days) 
+  tmp_last_session = start_date + datetime.timedelta(days=random_number_of_days)
+  
+  # Dans boucle => Générer dict tmp
+  tmp_name = choice(names)
+  tmp = {
+    "name": tmp_name.lower(),
+    "email": tmp_name.lower() + "@" + choice(domains),
+    "last_payment": tmp_last_payment.strftime("%Y-%m-%d"),
+    "last_session": tmp_last_session.strftime("%Y-%m-%d"),
+    "program": choice(programs),
+    "sponsorship": choice([True, False]),
+    "location": choice(locations)
+  }
+
+  # Dans boucle + Dict généré => Ajouter dict à db via .append()
+  db.append(tmp)
+# Fin boucle
+
+# Afficher db via print()
+# pprint(db)
+print(pd.DataFrame(db))
